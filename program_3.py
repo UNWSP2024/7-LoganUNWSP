b1cd8719759ef8605f46948ebe754476a168e657
LoganUNWSP <lkholmstrom@students.unwsp.edu>
2025-03-07 21:15:37 -0600

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -1,25 +1,31 @@
-# Program #3: US_Population
-def main():
-    # Have the user input (using a loop) various information that contains three pieces of data: 
-    # year, name of state, and population.  
-    # Store all of this information in a list of lists.  For example it might be stored like this:
-    
-    # [[2010, "Maine", 1987435], [2010,"Minnesota",6873202], [2011, "Iowa", 3421988]]
-    all_entered_values = []
+# Logan H's US Population
 
-    # Now have the user enter a year. 
-    
-    # The program will add the populations from all states in the list of list for that year only.
-    # Pass the list and year to the sum_population_for_year
+# 1st, SET our data list.
+data_list = []
 
-def sum_population_for_year(all_entered_values, year_to_sum):
-    # Loop through and sum the populations for the appropriate year. 
-    # e.g. for the list on line 7 the total would be 8,860,637 if the user enterd 2010 for the year to sum,
-    # or 3,421,988 if they enterd 2011 for the year to sum.
+# 2nd, ASK the year, this and step 4 will still happen as long as positive year is inputted.
+while True:
+    year = int(input("Enter the year (Enter a Negative to end): "))
 
-    # print the totalled population
+# 3rd, BREAK LOOP if a negative is entered.
+    if year <= -1:  # Exit condition for the loop
+        break
 
+# 4th, ASK the state and population.
+    state = input("Enter the name of the state: ")
+    population = int(input(f"Enter the population of {state}: "))
 
-# Call the main function.
-if __name__ == '__main__':
-    main()
+# 5th, STORE the data in the list of lists.
+    data_list.append([year, state, population])
+
+# 6th, WHEN LOOP IS BROKEN, ASK for the year they would like to search through.
+year_to_search = int(input("Enter the year to search for: "))
+
+# 7th, CALCULATE total population for given year.
+total_population = 0
+for data in data_list:
+    if data[0] == year_to_search:
+        total_population += data[2]
+
+# 8th, FINALLY< PRINT the result.
+print(f"Total population for the year {year_to_search}: {total_population}")
